fd9d3f03c7837da298337593c4ea081228b63b5a
Taranchuk <nfhfyxer@gmail.com>
2022-06-30 09:17:48 +0300

    update

---
+++ 1.3/Source/DeepStorageHelper.cs
@@ -0,0 +1,76 @@
+﻿using RimWorld;
+using System;
+using System.Linq;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace StackReservationFix
+{
+    public static class DeepStorageHelper
+    {
+        public static bool HasDeepStorageAndCanUse(Job job, Pawn hauler, IntVec3 cell)
+        {
+            bool isVanillaHaulingJob = job.def == JobDefOf.HaulToCell;
+            bool isPickupAndHaulJob = job.def.defName == "HaulToInventory";
+            if (isVanillaHaulingJob || isPickupAndHaulJob)
+            {
+                var buildingStorage = cell.GetFirstThing<Building_Storage>(hauler.Map);
+                if (buildingStorage != null)
+                {
+                    var comp = buildingStorage.GetComp<LWM.DeepStorage.CompDeepStorage>();
+                    if (comp != null)
+                    {
+                        var pawnHaulCount = 0;
+                        var otherPawns = hauler.Map.mapPawns.SpawnedPawnsInFaction(hauler.Faction).Where(x => x != hauler).Distinct();
+                        if (isVanillaHaulingJob)
+                        {
+                            var existingCapacityToStore = comp.CapacityToStoreThingAt(job.targetA.Thing, hauler.Map, cell);
+                            pawnHaulCount = Mathf.Min(hauler.CurJob.count, hauler.CurJob.targetA.Thing.stackCount);
+                            pawnHaulCount += otherPawns.Where(x => x.CurJob.def == JobDefOf.HaulToCell && x.CurJob.targetB.Cell == cell)
+                                            .Sum(x => Mathf.Min(x.CurJob.count, x.CurJob.targetA.Thing.stackCount));
+
+                            Log.Message("pawnHaulCount: " + pawnHaulCount);
+                            if (existingCapacityToStore - pawnHaulCount > 0)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                Log.Message("Can't stack on " + buildingStorage);
+                            }
+                        }
+                        else if (isPickupAndHaulJob)
+                        {
+                            pawnHaulCount = otherPawns.Where(x => x.CurJob.def.defName == "HaulToInventory" && x.CurJob.targetB.Cell == cell)
+                            .Sum(x => x.CurJob.countQueue.Sum());
+                            for (var i = 0; i < job.targetQueueA.Count; i++)
+                            {
+                                pawnHaulCount += Mathf.Min(job.countQueue[i], job.targetQueueA[i].Thing.stackCount);
+                                var existingCapacityToStore = comp.CapacityToStoreThingAt(job.targetQueueA[i].Thing, hauler.Map, cell);
+                                Log.Message(i + " - pawnHaulCount: " + pawnHaulCount);
+                                if (existingCapacityToStore - pawnHaulCount <= 0)
+                                {
+                                    Log.Message(i + " - Can't stack on " + buildingStorage);
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+        private static int TotalThingCountPickUpAndHaul(Thing thing, int count, Pawn hauler, IntVec3 cell, int existingCapacityToStore)
+        {
+            var otherPawns = hauler.Map.mapPawns.SpawnedPawnsInFaction(hauler.Faction).Where(x => x != hauler
+                && x.CurJob.def == JobDefOf.HaulToCell && x.CurJob.targetB.Cell == cell).Distinct().ToList();
+            var total = otherPawns.Sum(x => Mathf.Min(x.CurJob.count, x.CurJob.targetA.Thing.stackCount));
+            Log.Message($"hauler: {hauler}, hauler.CurJob: {hauler.CurJob}, hauler.CurJob.count: {hauler.CurJob.count}," +
+                $" thing: {thing}, otherReservations: {otherPawns.Count}, " +
+                $"existingCapacityToStore: {existingCapacityToStore}, count: {count}, total: {total}");
+            return total + Mathf.Min(count, hauler.CurJob.targetA.Thing.stackCount);
+        }
+    }
+}
---
+++ 1.3/Source/StackReservationFixMod.cs
@@ -0,0 +1,164 @@
+﻿using HarmonyLib;
+using PickUpAndHaul;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace StackReservationFix
+{
+    public class StackReservationFixMod : Mod
+    {
+        public static bool deepStorageLoaded;
+        public static Harmony harmony;
+        public StackReservationFixMod(ModContentPack pack) : base(pack)
+        {
+            harmony = new Harmony("StackReservationFix.Mod");
+            harmony.PatchAll();
+            deepStorageLoaded = ModsConfig.IsActive("LWM.DeepStorage");
+        }
+    }
+
+    [HarmonyPatch(typeof(Pawn_JobTracker), "StartJob")]
+    public class StartJobPatch
+    {
+        private static void Prefix(Pawn_JobTracker __instance, Pawn ___pawn, Job newJob, JobTag? tag)
+        {
+            try
+            {
+                if (___pawn.IsColonist)
+                {
+                    ___pawn.jobs.debugLog = true;
+                }
+            }
+            catch { }
+        }
+    }
+    
+    
+    
+    [HarmonyPatch(typeof(Pawn_JobTracker), "EndCurrentJob")]
+    public class EndCurrentJobPatch
+    {
+        private static void Prefix(Pawn_JobTracker __instance, Pawn ___pawn, JobCondition condition, ref bool startNewJob, bool canReturnToPool = true)
+        {
+            try
+            {
+                if (___pawn.IsColonist)
+                {
+                    Log.Message(___pawn + " is ending " + ___pawn.CurJob);
+                }
+            }
+            catch { };
+        }
+    }
+    
+    [HarmonyPatch(typeof(ThinkNode_JobGiver), "TryIssueJobPackage")]
+    public class TryIssueJobPackage
+    {
+        private static void Postfix(ThinkNode_JobGiver __instance, ThinkResult __result, Pawn pawn, JobIssueParams jobParams)
+        {
+            try
+            {
+                if (pawn.IsColonist && __result.Job != null)
+                {
+                    Log.Message(pawn + " gets " + __result.Job + " from " + __instance);
+                }
+            }
+            catch { }
+        }
+    }
+    
+    [StaticConstructorOnStartup]
+    public static class Startup2
+    {
+        static Startup2()
+        {
+            var postfix = AccessTools.Method(typeof(Startup2), "Postfix");
+            foreach (var type2 in typeof(ThinkNode).AllSubclassesNonAbstract())
+            {
+                var methodToPatch2 = AccessTools.Method(type2, "TryIssueJobPackage");
+                try
+                {
+                    StackReservationFixMod.harmony.Patch(methodToPatch2, postfix: new HarmonyMethod(postfix));
+                }
+                catch { }
+            }
+            var postfix2 = AccessTools.Method(typeof(Startup2), "Postfix2");
+            foreach (var type3 in typeof(ThinkNode_Conditional).AllSubclassesNonAbstract())
+            {
+                var methodToPatch3 = AccessTools.Method(type3, "Satisfied");
+                try
+                {
+                    StackReservationFixMod.harmony.Patch(methodToPatch3, postfix: new HarmonyMethod(postfix2));
+                }
+                catch { }
+            }
+            
+            var postfix3 = AccessTools.Method(typeof(Startup2), "Postfix3");
+            foreach (var type3 in typeof(WorkGiver_Scanner).AllSubclassesNonAbstract())
+            {
+                var methodToPatch3 = AccessTools.Method(type3, "HasJobOnThing");
+                try
+                {
+                    StackReservationFixMod.harmony.Patch(methodToPatch3, postfix: new HarmonyMethod(postfix3));
+                }
+                catch { }
+            }
+            
+            var postfix4 = AccessTools.Method(typeof(Startup2), "Postfix4");
+            foreach (var type3 in typeof(WorkGiver_Scanner).AllSubclassesNonAbstract())
+            {
+                var methodToPatch3 = AccessTools.Method(type3, "JobOnThing");
+                try
+                {
+                    StackReservationFixMod.harmony.Patch(methodToPatch3, postfix: new HarmonyMethod(postfix4));
+                }
+                catch { }
+            }
+        }
+    
+        private static void Postfix(ThinkNode __instance, ThinkResult __result, Pawn pawn, JobIssueParams jobParams)
+        {
+            if (pawn.IsColonist)
+            {
+                Log.Message(pawn + " gets " + __result.Job + " from " + __instance);
+                if (__instance is ThinkNode_Subtree subtree)
+                {
+                    Log.Message("Subtree: " + subtree.treeDef);
+                }
+            }
+        }
+        
+        private static void Postfix2(ThinkNode_Conditional __instance, bool __result, Pawn pawn)
+        {
+            if (pawn.IsColonist)
+            {
+                Log.Message(pawn + " gets " + __result + " from " + __instance);
+            }
+        }
+        
+        private static void Postfix3(WorkGiver_Scanner __instance, bool __result, Pawn pawn, Thing thing, bool forced = false)
+        {
+            if (pawn.IsColonist)
+            {
+                Log.Message(pawn + " gets " + __result + " from " + __instance);
+                if (__instance is WorkGiver_HaulToInventory haul)
+                {
+                    Log.Message("1: " + WorkGiver_HaulToInventory.OkThingToHaul(thing, pawn));
+                    Log.Message("2: " + WorkGiver_HaulToInventory.IsNotCorpseOrAllowed(thing));
+                    Log.Message("3: " + HaulAIUtility.PawnCanAutomaticallyHaulFast(pawn, thing, forced));
+                    Log.Message("4: " + StoreUtility.TryFindBestBetterStorageFor(thing, pawn, pawn.Map, StoreUtility.CurrentStoragePriorityOf(thing), pawn.Faction, out _, out _, false));
+                }
+            }
+        }
+        
+        private static void Postfix4(WorkGiver_Scanner __instance, Job __result, Pawn pawn)
+        {
+            if (pawn.IsColonist)
+            {
+                Log.Message(pawn + " gets " + __result + " from " + __instance);
+            }
+        }
+    }
+}
